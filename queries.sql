4a2b759fa0a160596bb59ca561ae4beff8f7b1d4
Carly Richardson <cbrich16@gmail.com>
2022-04-20 20:56:05 -0500

    Added queries and retirement info.

---
+++ queries.sql
@@ -0,0 +1,56 @@
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no TEXT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+DROP TABLE titles CASCADE;
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title TEXT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
